f4c9b3e94e7f0cfdb62b2ea23e28997a39d6a32e
Ian <hello@ian.pe>
2021-07-13 16:28:16 +0100

    Fix append/prepend data for scss import

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        appendData: `@import "@/assets/scss/app.scss";`
+        prependData: `@import "@/assets/scss/app.scss";`
       }
     }
   }
